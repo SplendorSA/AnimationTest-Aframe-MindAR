c24df72ca630fa9053bdf551371893c390a3ac12
MtrQ <130695828+SplendorSA@users.noreply.github.com>
2023-12-21 14:30:20 -0500

    a

--- assets/js/mindarscripting.js
+++ assets/js/mindarscripting.js
@@ -1,7 +1,91 @@
-// Función de creación, aparición y eventos de los botones
-const ShowButtons = () => {
 
-}
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("avatar").setAttribute('animation-mixer','clip: Idle; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+  
+  let isCenter = true;
+  let isLeft = false;
+  let isRight = false;
+
+
+  const leftBtn = document.getElementById("leftBtn");
+  const centerBtn = document.getElementById("centerBtn");
+  const rightBtn = document.getElementById("rightBtn");
+
+
+  leftBtn.addEventListener("click", function(){
+
+    if(isCenter){
+      
+      document.getElementById("avatar").setAttribute('animation-mixer','clip: CenterToIzq; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+      
+      isCenter = false;
+      isRight = false;
+      isLeft = true;
+
+    }
+
+    if(isRight){
+
+      document.getElementById("avatar").setAttribute('animation-mixer','clip: DerToIzq; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+      isCenter = false;
+      isRight = false;
+      isLeft = true;
+
+    }
+
+
+  });
+
+  centerBtn.addEventListener("click", function(){
+    
+    if(isRight){
+      
+      document.getElementById("avatar").setAttribute('animation-mixer','clip: DerToCenter; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+      
+      isCenter = true;
+      isRight = false;
+      isLeft = false;
+
+    }
+    
+    if(isLeft){
+      
+      document.getElementById("avatar").setAttribute('animation-mixer','clip: IzqToCenter; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+      
+      isCenter = true;
+      isRight = false;
+      isLeft = false;
+
+    }
+
+  });
+
+  rightBtn.addEventListener("click", function(){
+    
+    if(isCenter){
+      
+      document.getElementById("avatar").setAttribute('animation-mixer','clip: CenterToDer; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+      
+      isCenter = false;
+      isRight = true;
+      isLeft = false;
+
+    }
+
+    if(isLeft){
+      
+      document.getElementById("avatar").setAttribute('animation-mixer','clip: IzqToDer; loop: once; clampWhenFinished: true; crossFadeDuration: 3;');
+      
+      isCenter = false;
+      isRight = true;
+      isLeft = false;
+
+    }
+
+  });
+
+});
 
 //Eventos al registrar el Target
 AFRAME.registerComponent('mytarget', {
@@ -9,19 +93,26 @@ AFRAME.registerComponent('mytarget', {
   //Se inicia esta función cuando el Target es encontrado
   init: function () {
 
+    const DivConBotones = document.getElementById("Aparece-en-Escena");
+
     // Hace la lectura de la señal de que el Target fue encontrado
     this.el.addEventListener('targetFound', event => {
 
       // Avisa en la consola que el Target fue encontrado
       console.log("Target Found");
 
-      });
+      DivConBotones.style.display = "";
 
-      // Hace la lectura de la señal de que el Target se perdió
-      this.el.addEventListener('targetLost', event => {
+    });
 
-        //Avisa en la consola que el Target se perdió
+    // Hace la lectura de la señal de que el Target se perdió
+    this.el.addEventListener('targetLost', event => {
+
+      //Avisa en la consola que el Target se perdió
       console.log("Target Lost");
+
+      DivConBotones.style.display = "none";
+
 
     });
 
